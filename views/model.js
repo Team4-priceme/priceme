c15694f72d5174e439eb10d8362d13a8e953903a
Moana Stirling <moana.stirling@gmail.com>
2016-08-14 10:54:04 +1200

    model.js changes

--- views/model.js
+++ views/model.js
@@ -1,10 +1,17 @@
-$.ajax({
-  url: "/getUsedData",
-  data: {
+var make = "Toyota";
+var model = "RAV4";
+var yearMin = 0;
+var yearMax = 2016;
 
-  },
-  method: "GET",
-  success: function(res) {
-    console.log(res);
-  }
-});
+function search(make, model, yearMin, yearMax){
+  $.ajax({
+    url: "/getUsedData",
+    data: {
+      make:make, model:model, yearMin:yearMin, yearMax:yearMax
+    },
+    method: "GET",
+    success: function(res) {
+      console.log(res);
+    }
+  });
+}
