88f6f8f0c725329e5e4077c21f6f72639cab4005
Moana Stirling <moana.stirling@gmail.com>
2016-08-14 10:37:49 +1200

    app.js changes

--- public/javascript/getData.js
+++ public/javascript/getData.js
@@ -6,7 +6,7 @@ function getUsedCars(make, model, yearMin, yearMax, callback){
   var options = {
   url: sprintf('https://api.tmsandbox.co.nz/v1/Search/Motors/Used.json?make=%s&model=%s&year_max=%d&year_min=%d', make, model, yearMax, yearMin),
   headers: {
-    'Authorization': sprintf('OAuth oauth_consumer_key=%s, oauth_signature_method="PLAINTEXT", oauth_signature=%s', process.env.TOKEN, process.env.SECRET)
+    'Authorization': sprintf('OAuth oauth_consumer_key=%s, oauth_signature_method="PLAINTEXT", oauth_signature=%s&', process.env.TOKEN, process.env.SECRET)
   }
 };
 
