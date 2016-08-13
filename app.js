97148e12cb1fbd43d438d92c62a951d381334667
Justin Vos <justinvosnz@gmail.com>
2016-08-13 15:35:21 +1200

    Fixed default port

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var app = express();
 
 app.get('/', function (req, res) {
   //res.send('Hello World!');
+
+  console.log("default entry point");
   res.sendFile(path.join(__dirname + "/views/index.html"));
 });
 
@@ -13,6 +15,6 @@ app.get('/', function (req, res) {
 app.use(express.static(__dirname + '/views'));
 
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
 });
