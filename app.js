0519605757309c79f0d7bc6d410d64a4e2e795aa
Moana Stirling <moana.stirling@gmail.com>
2016-08-13 22:15:11 +1200

    Some framework for tomorrow

--- app.js
+++ app.js
@@ -4,17 +4,23 @@ var getData = require("./public/javascript/getData.js");
 
 var app = express();
 
+var make = "Toyota";
+var model = "RAV4";
+var yearMin = 0;
+var yearMax = 2016;
+
 app.get('/', function (req, res) {
   console.log("default entry point");
   res.sendFile(path.join(__dirname + "/views/index.html"));
 });
 
-app.get('/getCarData', function(req, res){
-  var make = "Toyota";
-  var model = "RAV4";
-  var yearMin = 0;
-  var yearMax = 2016;
+app.get('/sendingData', function(req, res){
+  /*http://mherman.org/blog/2013/10/20/handling-ajax-calls-with-node-dot-js-and-express-scraping-craigslist/#.V67mmCh97IU
+  var val = req.query.sendingData
+  console.log(val);*/
+});
 
+app.get('/getCarData', function(req, res){
   getData.getUsedCars(make, model, yearMin, yearMax, function(cars){
     var count = 0;
     var total = 0;
