b918075995952cdf8b623f42af2ae77c828c9215
Moana Stirling <moana.stirling@gmail.com>
2016-08-13 15:02:16 +1200

    test.js changes

--- test.js
+++ test.js
@@ -1,22 +1,21 @@
 var request = require('request');
 var sprintf = require("sprintf-js").sprintf,
-    vsprintf = require("sprintf-js").vsprintf
+    vsprintf = require("sprintf-js").vsprintf;
 
 function cacheCars(make, model, yearMin, yearMax){
-  var requestURL = sprintf('https://trademe-api-proxy.herokuapp.com/v1/Search/Motors/Used.json?make=%s&model=%s&year_max=%d&year_min=%d',
-  make, model, yearMin, yearMax)
-  console.log(requestURL)
+  var requestURL = sprintf('https://trademe-api-proxy.herokuapp.com/v1/Search/Motors/Used.json?make=%s&model=%s&year_max=%d&year_min=%d',make, model, yearMin, yearMax);
+  console.log(requestURL);
   request(requestURL, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       var cars = JSON.parse(body)['List'];
-      console.log(cars)
-      return cars
+      console.log(cars);
+      return cars;
     }
   })
 }
 
-var cars = cacheCars('toyota', 'rav4', 2000, 2014)
-console.log(cars)
+var cars = cacheCars('toyota', 'rav4', 2000, 2014);
+console.log(cars);
 /*
 for (i= 0; i < cars.length; i++) {
   car = cars[i]
