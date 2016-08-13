5a55c436153e870b1776b9b298a48661284f5240
Moana Stirling <moana.stirling@gmail.com>
2016-08-13 15:44:32 +1200

    changes

--- productDataRetrieval.js
+++ productDataRetrieval.js
@@ -2,17 +2,16 @@ var request = require('request');
 var sprintf = require("sprintf-js").sprintf,
     vsprintf = require("sprintf-js").vsprintf;
 
-function cacheCars(make, model, yearMin, yearMax){
+function cacheCars(make, model, yearMin, yearMax, callback){
   var requestURL = sprintf('https://trademe-api-proxy.herokuapp.com/v1/Search/Motors/Used.json?make=%s&model=%s&year_max=%d&year_min=%d', make, model, yearMax, yearMin);
   request(requestURL, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       var cars = JSON.parse(body)['List'];
-      return cars
+      callback(cars);
     }
   })
 }
 
-console.log(cacheCars('toyota', 'rx8', 1999, 2016))
 exports.cacheCars = cacheCars;
 /*
 
