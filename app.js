811484adc47818931acdbe2732c745e9e7d6f123
Justin Vos <justinvosnz@gmail.com>
2016-08-14 13:00:04 +1200

    Refactored back-end code.

--- app.js
+++ app.js
@@ -23,35 +23,7 @@ app.get('/getUsedData', function(req, res){
 
   console.log(make, model, yearMin, yearMax);
 
-  getData.getUsedCars(make, model, yearMin, yearMax, function(cars) {
-    console.log(1);
-    var buyCount = 0;
-    var buyTotal= 0;
-    var total = 0;
-    for (var i=0; i < cars.length; i++) {
-      if (cars[i].hasOwnProperty('BuyNowPrice')) {
-        buyCount += 1;
-        buyTotal += cars[i].BuyNowPrice
-      }
-      cars[i].Price = parseInt(cars[i].PriceDisplay.replace(/\D/g, ""));
-      total += cars[i].Price;
-    }
-
-    console.log(2);
-
-    if (total === 0) {
-      var jsonStr = JSON.stringify({info:{averageAsking:0, averageBuyNow:0, make:make, model:model, yearMin:yearMin, yearMax:yearMax}, cars:cars});
-    }
-    else {
-      if (buyTotal != 0) {
-        var jsonStr = JSON.stringify({info:{averageAsking:total/cars.length, averageBuyNow:buyTotal/buyCount,  make:make, model:model, yearMin:yearMin, yearMax:yearMax}, cars:cars});
-      }
-      else {
-        var jsonStr = JSON.stringify({info:{averageAsking:total/cars.length, averageBuyNow:0,  make:make, model:model, yearMin:yearMin, yearMax:yearMax}, cars:cars});
-      }
-    }
-    console.log(3);
-
+  getData.getUsedCars(make, model, yearMin, yearMax, function(jsonStr) {
     res.send(jsonStr);
   });
 });
--- public/javascript/getData.js
+++ public/javascript/getData.js
@@ -14,7 +14,32 @@ function getUsedCars(make, model, yearMin, yearMax, callback){
   request(options, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       var cars = JSON.parse(body)['List'];
-      callback(cars);
+
+      var buyCount = 0;
+      var buyTotal= 0;
+      var total = 0;
+      for (var i=0; i < cars.length; i++) {
+        if (cars[i].hasOwnProperty('BuyNowPrice')) {
+          buyCount += 1;
+          buyTotal += cars[i].BuyNowPrice
+        }
+        cars[i].Price = parseInt(cars[i].PriceDisplay.replace(/\D/g, ""));
+        total += cars[i].Price;
+      }
+
+      if (total === 0) {
+        var jsonStr = JSON.stringify({info:{averageAsking:0, averageBuyNow:0, make:make, model:model, yearMin:yearMin, yearMax:yearMax}, cars:cars});
+      }
+      else {
+        if (buyTotal != 0) {
+          var jsonStr = JSON.stringify({info:{averageAsking:total/cars.length, averageBuyNow:buyTotal/buyCount,  make:make, model:model, yearMin:yearMin, yearMax:yearMax}, cars:cars});
+        }
+        else {
+          var jsonStr = JSON.stringify({info:{averageAsking:total/cars.length, averageBuyNow:0,  make:make, model:model, yearMin:yearMin, yearMax:yearMax}, cars:cars});
+        }
+      }
+
+      callback(jsonStr);
     } else {
       console.log(response.statusCode);
     }
