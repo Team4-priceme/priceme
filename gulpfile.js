3ac831169066340340dc12bf04855374971227fd
Justin Vos <justinvosnz@gmail.com>
2016-09-28 22:21:48 +1300

    Added start task in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var run = require("gulp-run");
 
-gulp.task('sass', function () {
-  return gulp.src('./views/styles/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./views/styles'));
+gulp.task("start", ["sass"], function() {
+	return run("npm start").exec()
+	.pipe(gulp.dest('logs'));
+});
+
+gulp.task("sass", function() {
+  return gulp.src("./views/styles/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./views/styles"));
 });
